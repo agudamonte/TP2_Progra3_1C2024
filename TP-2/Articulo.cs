72c0e0e009de75743ed59a670d332c012f269709
lucas <lucas.saputo@gmail.com>
2024-04-17 18:15:01 -0300

    test cambio atributo

--- TP-2/Articulo.cs
+++ TP-2/Articulo.cs
@@ -10,7 +10,7 @@ namespace TP_2
     {
         public int ID { get; set; }
         public string Nombre { get; set; }
-        public string Descripcion { get; set; }
+        public string Descripcionn { get; set; }
         public string Codigo { get; set; }
         public Marca Marca { get; set; }
         public Categoria Categoria { get; set; }
